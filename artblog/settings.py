3ec0363788434f867a2170769d54f64c5f666d09
Kimmy <kodirovairoda7@gmail.com>
2021-06-21 22:37:08 +0500

    login

--- artblog/settings.py
+++ artblog/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.urls import reverse_lazy
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -123,7 +125,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+LOGIN_URL = reverse_lazy('login')
 try:
     from .local_settings import *
 except ImportError:
